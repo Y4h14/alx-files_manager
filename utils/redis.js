8c4a92e677d4c7971a9dd2db00873cfda632c92d
Y4h14 <yahyasadig2013@gmail.com>
2024-08-01 14:53:56 +0300

    added redis util

--- utils/redis.js
+++ utils/redis.js
@@ -1,1 +1,63 @@
 #!/usr/bin/node
+const { createClient } = require('redis');
+const { promisify } = require('util');
+
+class RedisClient {
+  // constructor creating a redis client.
+  constructor() {
+    this.client = createClient()
+      .on('ready', () => {
+      })
+      .on('error', (err) => {
+        console.log(err);
+      });
+
+    this.getAsync = promisify(this.client.GET).bind(this.client);
+    this.setAsync = promisify(this.client.SET).bind(this.client);
+    this.delAsync = promisify(this.client.DEL).bind(this.client);
+  }
+
+  // isAlive return ture if connection successful.
+  async isAlive() {
+    try {
+      const response = await this.client.ping();
+      return response === 'PONG';
+    } catch (err) {
+      console.error(`Connection Error: ${err}`);
+      return false;
+    }
+  }
+
+  // async get function
+  async get(key) {
+    try {
+      const result = await this.getAsync(key);
+      return result;
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  // async set fucntion
+  async set(key, value, duration) {
+    try {
+      await this.setAsync(key, value);
+      await this.client.expire(key, duration);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  // async del function
+  async del(key) {
+    try {
+      const response = await this.client.DEL(key);
+      console.log(response);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+}
+
+const redisClient = new RedisClient();
+module.exports = redisClient;
